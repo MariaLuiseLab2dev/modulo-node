110be6faa2733c8492110984cfe4134cb5287768
MariaLuiseLab2dev <maria.luise@lab2dev.com>
2025-08-26 10:16:22 -0300

    feature:adding module 5

--- modulo-5/atvd-1/index.js
+++ modulo-5/atvd-1/index.js
@@ -5,6 +5,7 @@ const port = 8080;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); // body em formato json
 app.use(express.static("static"));
 
 const lista = [
@@ -25,23 +26,26 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/produto', (req, res) => {
-    if (!nome || !categoria) {
-        return res.status(400).send('Informe nome e categoria');
-    }
+    // verificando se veio algo no body pra não vim vazio
+    if (!req.body.nome || !req.body.categoria) {
+        res.status(400).send('Informe nome e categoria');
+    } else {
 
+    // adiciono a lista 
     lista.push(req.body);
     console.log(req.body);
     res.status(201).send("Produto atualizado!");
+    }
 });
 
 app.get('/view-produtos', (req, res) => {
-    res.json(lista);
+    res.json(lista); // imprimo a lista em formato json
 });
 
 app.listen(port, () => {
-    console.log(`API está na porta ${port}`)
+    console.log(`API está na porta ${port}`);
 });
 
 function login(email, senha) {
-    return email === "teste@teste.com" && senha === "123";
+    return email === "teste@teste.com" && senha === "123"; // retorna true ou false
 }
